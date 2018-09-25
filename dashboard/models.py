10d9cc75194037d70173f46dc64ea498631f8767
doriclazar <contact@doriclazar.net>
2018-09-25 03:21:27 +0000

    Contain settings.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete = models.DO_NOTHING, related_name='user_settings')
+    chart_capacity_type = models.CharField(max_length = 32, default='doghnut')
+    chart_comparison_type = models.CharField(max_length = 32, default='line')
+    chart_period = models.IntegerField(default=3)
+
+    class Meta:
+        verbose_name = "User Settings"
+        verbose_name_plural = "User Settings"
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,8 +4,12 @@ from profiles.models import UserProfile, GroupProfile
 from bots.models import Bot
 from library.models import Profession, Module, Category, Command
 from schedule.models import TimeSpan
+from .models import UserSettings
 
 def get_dashboard(request, **kwargs):
+
+    users_settigs = UserSettings.objects.filter(user = auth.user)
+
     kwargs['group_profiles'] = GroupProfile.objects.all()
     kwargs['user_profiles'] = UserProfile.objects.all()
     kwargs['professions'] = Profession.objects.all()
@@ -14,14 +18,23 @@ def get_dashboard(request, **kwargs):
     kwargs['commands'] = Command.objects.all()
     kwargs['timespans'] = TimeSpan.objects.all()
     kwargs['bots'] = Bot.objects.all()
-    kwargs['bots_numdata'] = []
-    for bot_data in kwargs['bots']:
-        worked_timespans =  TimeSpan.objects.filter(bot = bot_data, is_skip=False).values('start_day', 'end_day', 'start_date', 'end_date', 'start_time', 'end_time')
-        #bot_time_worked = timesince(bot_worked_timespans.date_start) - timesince(bot_worked_timespans.date_end) - 24*(7 - (end_day - start_day)) - (timesince(start_time) - timesince(end_time))
-        #bot_time_idled = 
-        #bot_time_free =
-        bot_numdata = [1, 1, 1]
-        kwargs['bots_numdata'].append(bot_numdata)
+
+    worked_timespans = []
+    for bot in kwargs['bots']:
+        for index in range(int(user_settigs[0]['chart_period'])):
+            pass
+        #worked_timespans.append(TimeSpan.objects.filter(bot = bot, end_date before 2m ago & after 3m ago, is_skip=False).values('id'))
+        #worked_timespans.append(TimeSpan.objects.filter(bot = bot, end_date before 2m ago & after 3m ago, is_skip=False).values('id'))
+        #worked_timespans.append(TimeSpan.objects.filter(bot = bot, end_date before 1m ago & after 2m ago, is_skip=False).values('id'))
+        #worked_timespans.append(TimeSpan.objects.filter(bot = bot, end_date before today & after 1m ago, is_skip=False).values('id'))
+        #for timespan in worked_timespans:
+        #    bot.numdata.append(len(timespan))
+        
+        #time_worked = timesince(worked_timespans.date_start) - timesince(worked_timespans.date_end) - 24*(7 - (worked_timespans.end_day - worked_timespans.start_day)) - (timesince(worked_timespans.start_time) - timesince(worked_timespans.end_time))
+
+    #for bot_index in range(len(kwargs['bots'])):
+        #worked_timespans =  TimeSpan.objects.filter(bot = kwargs['bots'][bot_index], is_skip=False).values('start_day', 'end_day', 'start_date', 'end_date', 'start_time', 'end_time')
+        #kwargs['bots'][bot_index]
     
     '''
     worked_timespans =  Schedule.objects.filter(is_skip=False).values('start_day, end_day, start_date, end_date')
